6427ffbbecd9f8d3db4ee771f4cec692cd5edf3d
villeve <vehnia.ville@gmail.com>
2019-09-13 10:50:27 +0300

    added frontend

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # node-and-express
 Part 03 of fullstackopen (https://fullstackopen.com/osa3/node_js_ja_express)
+
+Link to site deployed on Heroku:
+https://powerful-fjord-13341.herokuapp.com/
---
+++ build/precache-manifest.898b081cae6c2a4f06e21d4745b358c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1726ba82bbe66279ab47aeb491c3373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c1389b2cbc8fbb31008",
+    "url": "/static/css/main.76e0ae89.chunk.css"
+  },
+  {
+    "revision": "c4411931316cbabd8c1a",
+    "url": "/static/js/2.987c573a.chunk.js"
+  },
+  {
+    "revision": "9c1389b2cbc8fbb31008",
+    "url": "/static/js/main.b5b5bb63.chunk.js"
+  },
+  {
+    "revision": "f66624ef4f93d72b8c01",
+    "url": "/static/js/runtime~main.8cfc7186.js"
+  }
+]);
